24c9bd4025e44bfa89971ab4f05b18b14671f559
ThatLukeDev <luke@l2j.co.uk>
2023-10-24 07:48:22 +0100

    added ray colour placeholder code for some nice image result (lens distortion)

--- ray.h
+++ ray.h
@@ -7,14 +7,15 @@ struct ray {
         ray() {}
         ray(vector3 originIn, vector3 directionIn) {
                 origin = originIn;
-                direction = directionIn;
+                direction = directionIn.unit();
         }
         vector3 at(double distance) {
                 return origin + direction * distance;
                 return origin + direction * distance;
         }
         color traceColor() {
-                return color();
+		vector3 rayAt1 = at(1);
+                return color(rayAt1.X, rayAt1.Y, rayAt1.Z);
         }
 };
 inline ostream& operator <<(ostream& out, ray r) {
