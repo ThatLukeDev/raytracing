12aa14a2a957a98b2e7343bc8053554dfbee2667
ThatLukeDev <luke@l2j.co.uk>
2023-10-25 17:59:38 +0100

    non working sphere intersection code

--- sphere.h
+++ sphere.h
@@ -3,20 +3,39 @@
 
 struct sphere {
 	vector3 position = vector3();
-	double r = 0;
+	double radius = 0;
 
 	sphere() {}
 
-	sphere(vector3 v, double rIn) {
+	sphere(vector3 v, double r) {
 		position = v;
-		r = rIn;
+		radius = r;
 	}
-	sphere(double x, double y, double z, double rIn) {
+	sphere(double x, double y, double z, double r) {
 		position = vector3(x, y, z);
-		r = rIn;
+		radius = r;
 	}
 
+	double intersectsAlong(ray r) {
+		vector3 SminusO = r.origin - position;
+		double a = dot(r.direction, r.direction);
+		double b = 2.0 * dot(r.direction, SminusO);
+		double c = dot(SminusO, SminusO) - radius * radius;
+		double underRoot = b*b - 4 * a * c;
+		if (underRoot < 0.0) {
+			return -1;
+		}
+		else if (underRoot == 0.0) {
+			return (-b) / 2*a;
+		}
+		else {
+			return (-b - 4*a*c) / 2*a;
+		}
+	}
 	bool intersects(ray r) {
-		return true;
+		if (intersectsAlong(r) != -1) {
+			return true;
+		}
+		return false;
 	}
 };
