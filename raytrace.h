5ba0e13536d9a869de661790d663c054ebead45f
ThatLukeDev <luke@l2j.co.uk>
2023-10-27 07:55:20 +0100

    objects now dont randomly choose zindex

--- raytrace.h
+++ raytrace.h
@@ -1,16 +1,21 @@
 #include <cmath>
 
 color traceColor(ray r) {
-	// placeholder code
 	vector3 rayAt1 = r.at(1);
 	vector3 hit = vector3(-1,-1,-1);
+	double intersectDsmallest = pow(2,32);
+	sphere* objectAtSmallest = nullptr;
 	for (int i = 0; i < objectsLength; i++) {
 		double intersectD = objects[i].intersectsAlong(r);
-		if (intersectD > 0) {
-			vector3 intersect = r.at(intersectD);
-			vector3 normal = (intersect - objects[i].position).unit();
-			return color(abs(normal.X), abs(normal.Y), abs(normal.Z));
+		if (intersectD > 0 && intersectD < intersectDsmallest) {
+			intersectDsmallest = intersectD;
+			objectAtSmallest = &objects[i];
 		}
+	}
+	if (intersectDsmallest != pow(2,32)) {
+		vector3 intersect = r.at(intersectDsmallest);
+		vector3 normal = (intersect - objectAtSmallest->position).unit();
+		return color(abs(normal.X), abs(normal.Y), abs(normal.Z));
 	}
 	return color(0, 0, 0);
 }
