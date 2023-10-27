131f6df76b124501a8bf7789667ddf050ae235e2
ThatLukeDev <luke@l2j.co.uk>
2023-10-27 07:28:52 +0100

    added normal calc code and used that to calculate sphere colour (looks interesting)

--- raytrace.h
+++ raytrace.h
@@ -7,7 +7,9 @@ color traceColor(ray r) {
 	for (int i = 0; i < objectsLength; i++) {
 		double intersectD = objects[i].intersectsAlong(r);
 		if (intersectD > 0) {
-			return color(1.0, 0.0, 0.0);
+			vector3 intersect = r.at(intersectD);
+			vector3 normal = (intersect - objects[i].position).unit();
+			return color(abs(normal.X), abs(normal.Y), abs(normal.Z));
 		}
 	}
 	return color(0, 0, 0);
