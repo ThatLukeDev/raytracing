24addfc20ae008b9517d1b54d2e7a016d0291b9f
ThatLukeDev <luke@l2j.co.uk>
2023-12-13 07:54:20 +0000

    refraction code mostly working

--- raytrace.h
+++ raytrace.h
@@ -5,7 +5,7 @@
 #include "pixel.h"
 #include "vector3.h"
 
-color traceColor(ray& r, int bounces, int maxBounces, environmentC environment, double falloff, randomDistribution& rnd) {
+color traceColor(ray& r, int bounces, int maxBounces, environmentC environment, double falloff, randomDistribution& rnd, object* ignore) {
 	if (bounces > maxBounces)
 		return color(0, 0, 0);
 
@@ -13,7 +13,7 @@ color traceColor(ray& r, int bounces, int maxBounces, environmentC environment, 
 	shared_ptr<object> closestObject = nullptr;
 	for (int i = 0; i < objectsLength; i++) {
 		double intersectD = objects[i]->intersectsAlong(r);
-		if (intersectD > 0.0) {
+		if (intersectD > 0.0 && &(*objects[i]) != ignore) {
 			if (intersectD < closestRay) {
 				closestRay = intersectD;
 				closestObject = objects[i];
@@ -29,12 +29,19 @@ color traceColor(ray& r, int bounces, int maxBounces, environmentC environment, 
 	vector3 intersect = r.at(closestRay);
 	vector3 normal = closestObject->normalAt(intersect);
 	vector3 randVector = vector3(rnd.randN(), rnd.randN(), rnd.randN()).unit();
-	ray bounce = ray(intersect, r.direction).reflect(normal).fuzz(randVector, 1 - closestObject->texture.fuzz);
+	ray bounce = ray(intersect, r.direction);
+	object* nextIgnore = nullptr;
+	if (rnd.randDouble() < closestObject->texture.transparency) { bounce = bounce.refract(normal, closestObject->texture.ior); }
+	else { bounce = bounce.reflect(normal); nextIgnore = &(*closestObject); }
+	bounce = bounce.fuzz(randVector, 1 - closestObject->texture.fuzz);
 
-	color bounceOutput = traceColor(bounce, bounces + 1, maxBounces, environment, falloff, rnd);
+	color bounceOutput = traceColor(bounce, bounces + 1, maxBounces, environment, falloff, rnd, nextIgnore);
 	color output = closestObject->texture.getPixel(bounceOutput);
 
 	return output;
+}
+color traceColor(ray& r, int bounces, int maxBounces, environmentC environment, double falloff, randomDistribution& rnd) {
+	return traceColor(r, bounces, maxBounces, environment, falloff, rnd, nullptr);
 }
 
 color traceColor(ray& r, int maxBounces, environmentC environment, double falloff, randomDistribution& rnd) {
